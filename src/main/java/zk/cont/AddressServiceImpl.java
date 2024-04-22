0ef01175e90ae27ecfa045e723839b4213b5012b
b.bubeev <b.bubeev@elg-sys.com>
2024-04-22 13:30:16 +0300

    есть конн с mysql

--- src/main/java/zk/cont/AddressServiceImpl.java
+++ src/main/java/zk/cont/AddressServiceImpl.java
@@ -1,32 +1,60 @@
 package zk.cont;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import zk.ent.Address;
 
+import java.sql.*;
 import java.util.LinkedList;
 import java.util.List;
 
 public class AddressServiceImpl implements AddressService{
+
+	private static final Logger log = LoggerFactory.getLogger(AddressServiceImpl.class);
+
+	private static final String SELECT_ADDRESS = "select id, _index, city, street, home_number, apartment from address";
+
+	private Connection getConnection() {
+		try {
+			Class.forName(Conn.DRIVER);
+			return DriverManager.getConnection(Conn.URL, Conn.USER, Conn.PASS);
+		} catch (ClassNotFoundException e) {
+			log.error("DriverClassNotFound :" , e);
+			throw new RuntimeException(e);
+		} catch (SQLException x) {
+			log.error("connect with {}, {} and his pass", Conn.URL, Conn.USER);
+			log.error("Exception :" , x);
+			throw new RuntimeException(x);
+		}
+	}
 
 	//data model
 	private List<Address> addressList = new LinkedList<Address>();
 	private static int id = 1;
 	//initialize book data
 	public AddressServiceImpl() {
-		addressList.add(
-				new Address(id++,
-						"670000",
-						"U-Ude",
-						"Zhukocskogo",
-						"21",
-						"13"));
 	}
 	
 	
 	public List<Address> findAll(){
-		return addressList;
+		List<Address> addresses = new LinkedList<>();
+		try (Connection con = getConnection();
+			 PreparedStatement stat = con.prepareStatement(SELECT_ADDRESS)) {
+			ResultSet rs = stat.executeQuery();
+			while (rs.next()) {
+				addresses.add(new Address(rs.getInt(1), rs.getString(2), rs.getString(3),
+						rs.getString(4), rs.getString(5), rs.getString(6)));
+			}
+			rs.close();
+		} catch (SQLException e) {
+			log.error("Select address exception :", e);
+		}
+		return addresses;
 	}
 	
 	public List<Address> search(String keyword){
+		addressList = findAll();
+		log.info("BULAT GOT FROM MYSQL :" + addressList.size());
 		List<Address> result = new LinkedList<Address>();
 		if (keyword==null || "".equals(keyword)){
 			result = addressList;
---
+++ src/main/java/zk/cont/Conn.java
@@ -0,0 +1,8 @@
+package zk.cont;
+
+public class Conn {
+    static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    static final String USER = "user";
+    static final String URL = "jdbc:mysql://127.0.0.1:3306/mydb?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    static final String PASS = "pw";
+}
